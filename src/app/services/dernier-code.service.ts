c7782e7ffaa63f822e6605fa55293d12ac9bed93
willy68 <william.lety@gmail.com>
2020-01-02 15:08:48 +0100

    Add services

---
+++ src/app/services/dernier-code.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DernierCode } from '../models';
+import { apigest } from '../url';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DernierCodeService {
+
+  constructor() { }
+}
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -6,3 +6,7 @@ export * from './entreprise.service';
 export * from './entreprise-storage.service';
 export * from './links.service';
 export * from './client.service';
+export * from './adresse-type.service';
+export * from './adresse.service';
+export * from './civilite.service';
+export * from './dernier-code.service';
