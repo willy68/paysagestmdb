d119c0f06cecea6919381cfb21f5614f4bee17ee
willy68 <william.lety@gmail.com>
2020-01-02 14:11:40 +0100

    Add models

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -2,3 +2,7 @@ export * from './role.enum';
 export * from './user';
 export * from './entreprise';
 export * from './client';
+export * from './adresse';
+export * from './adresse-type';
+export * from './civilite';
+export * from './dernier-code';
