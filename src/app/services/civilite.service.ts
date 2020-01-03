894ba0cfb0b59a9e04766aae07951608541a7d72
willy68 <william.lety@gmail.com>
2020-01-03 14:01:58 +0100

    Civilite service

--- src/app/services/civilite.service.ts
+++ src/app/services/civilite.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Civilite } from '../models';
+import { apigest } from '../url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CiviliteService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(params = '') {
+    return this.http.get<Civilite[]>(apigest + '/civilites' + params);
+  }
+
+  getList(entreprise_id: number, params = '') {
+    return this.http.get<Civilite[]>(apigest + `/entreprise/${entreprise_id}/civilite/list` + params);
+  }
+
+  get(entreprise_id: number, id: number, params = '') {
+    return this.http.get<Civilite>(apigest + `/entreprise/${entreprise_id}/civilite/${id}` + params);
+  }
+
+  create(entreprise_id: number, civilite: Civilite) {
+    return this.http.post<Civilite>(apigest + `/entreprise/${entreprise_id}/civilite`, civilite);
+  }
+
+  update(entreprise_id: number, civilite: Civilite) {
+    return this.http.put<Civilite>(apigest + `/entreprise/${entreprise_id}/civilite/${civilite.id}`, civilite);
+  }
+
+  delete(entreprise_id: number, id: number) {
+    return this.http.delete(apigest + `entreprise/${entreprise_id}/civilite/${id}`);
+  }
 }
--- src/app/services/dernier-code.service.ts
+++ src/app/services/dernier-code.service.ts
@@ -16,26 +16,26 @@ export class DernierCodeService {
   }
 
   getList(entreprise_id: number, params = '') {
-    return this.http.get<DernierCode[]>(apigest + `/entreprise/${entreprise_id}/client/list` + params);
+    return this.http.get<DernierCode[]>(apigest + `/entreprise/${entreprise_id}/dernier_code/list` + params);
   }
 
   get(entreprise_id: number, id: number, params = '') {
-    return this.http.get<DernierCode>(apigest + `/entreprise/${entreprise_id}/client/${id}` + params);
+    return this.http.get<DernierCode>(apigest + `/entreprise/${entreprise_id}/dernier_code/${id}` + params);
   }
 
   getLastCode(entreprise_id: number, table_nom: string) {
     return this.http.get<DernierCode>(apigest + `/entreprise/${entreprise_id}/dernier_code/${table_nom}`);
   }
 
-  create(entreprise_id: number, client: DernierCode) {
-    return this.http.post<DernierCode>(apigest + `/entreprise/${entreprise_id}/client`, client);
+  create(entreprise_id: number, dernier_code: DernierCode) {
+    return this.http.post<DernierCode>(apigest + `/entreprise/${entreprise_id}/dernier_code`, dernier_code);
   }
 
-  update(entreprise_id: number, client: DernierCode) {
-    return this.http.put(apigest + `/entreprise/${entreprise_id}/client/${client.id}`, client);
+  update(entreprise_id: number, dernier_code: DernierCode) {
+    return this.http.put(apigest + `/entreprise/${entreprise_id}/dernier_code/${dernier_code.id}`, dernier_code);
   }
 
   delete(entreprise_id: number, id: number) {
-    return this.http.delete(apigest + `entreprise/${entreprise_id}/client/${id}`);
+    return this.http.delete(apigest + `entreprise/${entreprise_id}/dernier_code/${id}`);
   }
 }
