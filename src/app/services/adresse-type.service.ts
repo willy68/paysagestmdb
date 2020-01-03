152c27d536ab97bd351360530dad1f76bd949c53
willy68 <william.lety@gmail.com>
2020-01-03 14:12:13 +0100

    AdresseType service

--- src/app/services/adresse-type.service.ts
+++ src/app/services/adresse-type.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AdresseType } from '../models';
+import { apigest } from '../url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdresseTypeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(params = '') {
+    return this.http.get<AdresseType[]>(apigest + '/adresse_types' + params);
+  }
+
+  getList(entreprise_id: number, params = '') {
+    return this.http.get<AdresseType[]>(apigest + `/entreprise/${entreprise_id}/adresse_type/list` + params);
+  }
+
+  get(entreprise_id: number, id: number, params = '') {
+    return this.http.get<AdresseType>(apigest + `/entreprise/${entreprise_id}/adresse_type/${id}` + params);
+  }
+
+  create(entreprise_id: number, adresseType: AdresseType) {
+    return this.http.post<AdresseType>(apigest + `/entreprise/${entreprise_id}/adresse_type`, adresseType);
+  }
+
+  update(entreprise_id: number, adresseType: AdresseType) {
+    return this.http.put<AdresseType>(apigest + `/entreprise/${entreprise_id}/adresse_type/${adresseType.id}`, adresseType);
+  }
+
+  delete(entreprise_id: number, id: number) {
+    return this.http.delete(apigest + `entreprise/${entreprise_id}/adresse_type/${id}`);
+  }
 }
