086ed873a0bab788331f88cf54772f87a04e3fa0
willy68 <william.lety@gmail.com>
2020-01-03 14:07:02 +0100

    Adresse service

--- src/app/services/adresse.service.ts
+++ src/app/services/adresse.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Adresse } from '../models';
+import { apigest } from '../url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdresseService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(params = '') {
+    return this.http.get<Adresse[]>(apigest + '/adresses' + params);
+  }
+
+  getList(entreprise_id: number, params = '') {
+    return this.http.get<Adresse[]>(apigest + `/entreprise/${entreprise_id}/adresse/list` + params);
+  }
+
+  get(entreprise_id: number, id: number, params = '') {
+    return this.http.get<Adresse>(apigest + `/entreprise/${entreprise_id}/adresse/${id}` + params);
+  }
+
+  create(entreprise_id: number, adresse: Adresse) {
+    return this.http.post<Adresse>(apigest + `/entreprise/${entreprise_id}/adresse`, adresse);
+  }
+
+  update(entreprise_id: number, adresse: Adresse) {
+    return this.http.put<Adresse>(apigest + `/entreprise/${entreprise_id}/adresse/${adresse.id}`, adresse);
+  }
+
+  delete(entreprise_id: number, id: number) {
+    return this.http.delete(apigest + `entreprise/${entreprise_id}/adresse/${id}`);
+  }
 }
