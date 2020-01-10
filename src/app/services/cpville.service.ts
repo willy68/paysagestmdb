9852a435574d280a1310e43549d6e7d4e3749780
William Lety <william.lety@gmail.com>
2020-01-10 18:25:39 +0100

    search function

--- src/app/services/cpville.service.ts
+++ src/app/services/cpville.service.ts
@@ -8,5 +8,17 @@ import { apigest } from '../url';
 })
 export class CpvilleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(params = '') {
+    return this.http.get<Cpville[]>(apigest + '/civilites' + params);
+  }
+
+  getList(entreprise_id: number, params = '') {
+    return this.http.get<Cpville[]>(apigest + `/entreprise/${entreprise_id}/civilite/list` + params);
+  }
+
+  search(column: string, searchTerm: string, params = '') {
+    return this.http.get<Cpville[]>(apigest + `cpville/${column}/${searchTerm}` + params)
+  }
 }
