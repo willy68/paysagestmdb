eaa65c4417a8906561f9b68254d6c460a2c13cc3
William Lety <william.lety@gmail.com>
2020-01-15 17:20:06 +0100

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { AuthenticationService } from './services';
-import { User, Role } from './models';
 
 @Component({
   selector: 'pg-root',
@@ -11,26 +7,7 @@ import { User, Role } from './models';
 })
 export class AppComponent {
   title = 'Paysagest';
-  currentUser: User;
 
-  constructor(
-    private router: Router,
-    private authenticationService: AuthenticationService
-  ) {
-    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-  }
-
-  get isAdmin() {
-    return this.currentUser && this.currentUser.role === Role.Admin;
-  }
-
-  get isRoot() {
-    return this.currentUser && this.currentUser.role === Role.Root;
-  }
-
-  logout() {
-    this.authenticationService.logout();
-    this.router.navigate(['/']);
-  }
+  constructor() {}
 
 }
