5937bf79456daa3e3c009370a309bcd44c4ed872
willy68 <william.lety@gmail.com>
2020-01-18 13:24:23 +0100

    Clean up

--- src/app/entreprise/entreprise/entreprise.component.ts
+++ src/app/entreprise/entreprise/entreprise.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-
-import { EntrepriseStorageService, AuthenticationService } from '../../services';
 
 @Component({
   selector: 'pg-entreprise',
@@ -10,40 +6,13 @@ import { EntrepriseStorageService, AuthenticationService } from '../../services'
   styleUrls: ['./entreprise.component.scss']
 })
 export class EntrepriseComponent implements OnInit, OnDestroy {
-  public entreprise_id: number = null;
-  public user_id: number = null;
-  private unsubscribAuthService: Subject<any> = new Subject<any>();
-  private unsubscribEntrStorageService: Subject<any> = new Subject<any>();
 
-  constructor( private entrepriseStorageService: EntrepriseStorageService,
-    private authenticationService: AuthenticationService) {
+  constructor() {
   }
 
   ngOnInit() {
-    this.entrepriseStorageService.entreprise
-    .pipe(takeUntil(this.unsubscribEntrStorageService))
-    .subscribe(x => {
-      if (x) {
-        this.entreprise_id = x.id;
-      } else {
-        this.entreprise_id = null;
-      }
-    });
-    this.authenticationService.currentUser
-    .pipe(takeUntil(this.unsubscribAuthService))
-    .subscribe(x => {
-      if (x) {
-        this.user_id = x.id;
-      } else {
-        this.user_id = null;
-      }
-    });
   }
 
   ngOnDestroy() {
-    this.unsubscribAuthService.next();
-    this.unsubscribAuthService.complete();
-    this.unsubscribEntrStorageService.next();
-    this.unsubscribEntrStorageService.complete();
   }
 }
