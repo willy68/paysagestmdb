6de79f8633fae9081c03802bbf7da7e49f68c9b4
William Lety <william.lety@gmail.com>
2020-03-16 18:26:26 +0100

    Refonte Reuse form

--- src/app/entreprise/client/client-update/client-update.component.ts
+++ src/app/entreprise/client/client-update/client-update.component.ts
@@ -13,105 +13,16 @@ import { Client, DernierCode } from 'src/app/models';
 })
 export class ClientUpdateComponent implements OnInit {
 
-  public submitted = false;
-  public loading = false;
-  public clientForm: FormGroup;
-  public client_code: Observable<DernierCode>;
-  private dernier_code: string;
-  private deadCreate = new Subject();
-  private entreprise_id: number;
-  private id: number;
-  private client: Client;
-  private updatedClient: Observable<Client>;
-
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private clientService: ClientService,
-    private dernierCodeService: DernierCodeService,
-    private alertService: AlertService) { }
+  addressForm: FormGroup;
+  client: Client;
+  constructor() { }
 
   ngOnInit() {
-    this.clientForm = new FormGroup({});
-
-    console.log(this.clientForm);
-
-    this.updatedClient = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => {
-        this.entreprise_id = +params.get('entreprise_id');
-        this.id = +params.get('id');
-        return this.clientService.get(this.entreprise_id, this.id).pipe(
-          tap((client: Client) => {
-            this.client = client;
-            this.clientForm.patchValue(client);
-          })
-        );
-      })
-    );
-    this.client_code = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => {
-        this.entreprise_id = +params.get('entreprise_id');
-        return this.dernierCodeService.getLastCode(this.entreprise_id, 'client').pipe(
-          tap(data => {
-            this.dernier_code = data.prochain_code;
-            // this.clientForm.patchValue({ code_client: this.dernier_code });
-          })
-        );
-      })
-    );
+    this.addressForm = new FormGroup({});
   }
 
-  // convenience getter for easy access to form fields
-  // get f() { return (<FormGroup>this.clientForm.controls['clientForm']); }
-  /*
-  get code_client() { return this.clientForm.get('code_client'); }
-  get civilite() { return this.clientForm.get('civilite'); }
-  get nom() { return this.clientForm.get('nom'); }
-  get prenom() { return this.clientForm.get('prenom'); }
-  get tel() { return this.clientForm.get('tel'); }
-  get portable() { return this.clientForm.get('portable'); }
-  get email() { return this.clientForm.get('email'); }
-  get tva_intracom() { return this.clientForm.get('tva_intracom'); }
-*/
   onSubmit() {
-    this.submitted = true;
-
-    console.log(this.clientForm.value);
-    // stop here if form is invalid or entreprise_id is null
-    if (this.clientForm.invalid || !this.entreprise_id) {
-      return;
-    }
-/*
-    this.loading = true;
-    this.clientService.update(this.entreprise_id, {
-      code_client: this.f.code_client.value,
-      civilite: this.f.civilite.value,
-      nom: this.f.nom.value,
-      prenom: this.f.prenom.value,
-      tel: this.f.tel.value,
-      portable: this.f.portable.value,
-      email: this.f.email.value,
-      tva_intracom: this.f.tva_intracom.value
-    })
-      .pipe(first(),
-      takeUntil(this.deadCreate))
-      .subscribe(
-        data => {
-          this.alertService.success('SUCCESS!! : ' + data.nom);
-          // navigue vers clients list
-          this.router.navigate(['../../'], {relativeTo: this.route});
-        },
-        error => {
-          this.alertService.error(error);
-        });
-    this.loading = false;*/
-  }
-
-  resetForm() {
-    this.submitted = false;
-    this.clientForm.reset({
-      code_client: { value: this.dernier_code, disabled: true }
-    });
+    console.log(this.addressForm.value);
   }
 
 }
--- src/app/forms/client/client-form/client-form.component.ts
+++ src/app/forms/client/client-form/client-form.component.ts
@@ -1,12 +1,6 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ControlContainer, FormGroupDirective, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { Client, DernierCode, Civilite } from 'src/app/models';
-import { Observable, BehaviorSubject, Subject, NEVER } from 'rxjs';
-import { DernierCodeService, CiviliteService } from 'src/app/services';
-import { MDBModalService, MDBModalRef } from 'angular-bootstrap-md';
-import { switchMap, tap, takeUntil } from 'rxjs/operators';
-import { ParamMap, ActivatedRoute } from '@angular/router';
-import { YesnomodalComponent } from 'src/app/yesnomodal/yesnomodal.component';
+import { Client } from '../../../models';
 
 @Component({
   selector: 'pg-client-form',
@@ -14,157 +8,40 @@ import { YesnomodalComponent } from 'src/app/yesnomodal/yesnomodal.component';
   styleUrls: ['./client-form.component.scss'],
   viewProviders: [{ provide: ControlContainer, useExisting: FormGroupDirective }]
 })
-export class ClientFormComponent implements OnInit, OnDestroy {
-  @Input() client: Client;
-  @Input() code: Observable<DernierCode>;
-  public entreprise_id: number;
-  form: FormGroup;
-  public dernier_code: string;
-  public client_code: Observable<DernierCode>;
-  private readonly refreshCiviliteList = new BehaviorSubject(null);
-  public civiliteList: Observable<Civilite[]>;
-  public civilites: Civilite[];
-  private deadModal = new Subject();
-  public createForm: FormGroup;
-  public submitted = false;
-  public loading = false;
-  public modalRef: MDBModalRef;
+export class ClientFormComponent implements OnInit {
 
+  @Input() client: Client;
+  form: FormGroup;
   constructor(
     private ctrlContainer: FormGroupDirective,
-    private fb: FormBuilder,
-    private route: ActivatedRoute,
-    private dernierCodeService: DernierCodeService,
-    private civiliteService: CiviliteService,
-    private modalService: MDBModalService) { }
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.form = this.ctrlContainer.form;
 
-    this.form.addControl('clientForm',
-      this.fb.group({
-        code_client: ['', [Validators.required]]/*,
-        civilite: ['', []],
-        nom: ['', [Validators.required]],
-        prenom: ['', []],
-        tel: ['', []],
-        portable: ['', []],
-        email: ['', [Validators.required, Validators.email]],
-        tva_intracom: ['', []]*/
-      })
-    );
-    // (<FormGroup>this.form.controls['clientForm']).patchValue({ code_client: 'CL1' });
-    console.log((<FormGroup>this.form.controls['clientForm']).controls);
-    console.log(this.form);
-/*
-    if (this.code) {
-      this.client_code = this.code.pipe(
-        tap(lastCode => (<FormGroup>this.form.controls['clientForm']).patchValue({ code_client: lastCode.prochain_code })
-      ));
-    } else {
-      this.client_code = this.route.paramMap.pipe(
-        switchMap((params: ParamMap) => {
-          this.entreprise_id = +params.get('entreprise_id');
-          return this.dernierCodeService.getLastCode(this.entreprise_id, 'client').pipe(
-            tap(data => {
-              this.dernier_code = data.prochain_code;
-              (<FormGroup>this.form.controls['clientForm']).patchValue({ code_client: this.dernier_code });
-            })
-          );
-        })
-      );
-    }
-*/
+    this.form.addControl('addressForm',
+      this.formBuilder.group({
+        'firstName': [null, [Validators.required]],
+        'lastName': [null, [Validators.required]],
+        'phone': [null, null],
+        'street': [null, [Validators.required]],
+        'city': [null, [Validators.required]],
+        'state': [null],
+        'zip': [null, [Validators.required]],
+      }));
 
-    this.civiliteList = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => {
-        this.entreprise_id = +params.get('entreprise_id');
-        return this.refreshCiviliteList.pipe(
-          switchMap(() => this.civiliteService.getList(this.entreprise_id).pipe(
-            tap(data => this.civilites = data)
-          ))
-        );
-      })
-    );
+    console.log(this.form);
   }
 
-  get f() { return (<FormGroup>this.form.controls['clientForm']).controls; }
-
-  get code_client() { return (<FormGroup>this.form.controls['clientForm']).get('code_client'); }
-  /*get civilite() { return (<FormGroup>this.form.controls['clientForm']).get('civilite'); }
-  get nom() { return (<FormGroup>this.form.controls['clientForm']).get('nom'); }
-  get prenom() { return (<FormGroup>this.form.controls['clientForm']).get('prenom'); }
-  get tel() { return (<FormGroup>this.form.controls['clientForm']).get('tel'); }
-  get portable() { return (<FormGroup>this.form.controls['clientForm']).get('portable'); }
-  get email() { return (<FormGroup>this.form.controls['clientForm']).get('email'); }
-  get tva_intracom() { return (<FormGroup>this.form.controls['clientForm']).get('tva_intracom'); }
-*/
   isInvalid(controlName: string): boolean {
-    return (<FormGroup>this.form.controls['clientForm']).controls[controlName].touched
-      && (<FormGroup>this.form.controls['clientForm']).controls[controlName].invalid;
+
+    return (<FormGroup>this.form.controls['addressForm']).controls[controlName].touched
+      && (<FormGroup>this.form.controls['addressForm']).controls[controlName].invalid;
   }
 
   isValid(controlName: string): boolean {
-    return (<FormGroup>this.form.controls['clientForm']).controls[controlName].touched
-      && (<FormGroup>this.form.controls['clientForm']).controls[controlName].valid;
-  }
-
-  civiliteFocusout(event) {
-    /*if (this.civilite.value.length &&
-      !this.civilites.find(element => element.libelle === this.civilite.value)) {
-      this.modalRef = this.openModal();
-      this.modalRef.content.action.pipe(
-        switchMap((result: any) => {
-          this.modalRef.hide();
-          if (result) {
-            return this.civiliteService.create(this.entreprise_id,
-              {
-                entreprise_id: this.entreprise_id,
-                libelle: this.civilite.value
-              });
-          } else {
-            return NEVER;
-          }
-        }),
-        takeUntil(this.deadModal))
-        .subscribe(() => this.refreshCiviliteList.next(null));
-    }*/
-  }
-
-  resetForm() {
-    this.submitted = false;
-    this.createForm.reset({
-      code_client: { value: this.dernier_code, disabled: true }
-    });
-  }
-
-  openModal()/*: MDBModalRef */{
-    /*return this.modalRef = this.modalService.show(YesnomodalComponent, {
-      backdrop: true,
-      keyboard: true,
-      focus: true,
-      show: false,
-      ignoreBackdropClick: false,
-      class: '',
-      containerClass: '',
-      animated: true,
-      data: {
-        heading: 'Sauvegarde',
-        content: {
-          heading: 'Civilité nouvelle',
-          description: 'Voulez sauvegarder la civilité ' + this.civilite.value
-        },
-        button: {
-          yes: 'Sauver!',
-          no: 'Fermer'
-        }
-      }
-    });*/
-  }
-
-  ngOnDestroy() {
-    this.deadModal.next();
-    this.deadModal.complete();
+    return (<FormGroup>this.form.controls['addressForm']).controls[controlName].touched
+      && (<FormGroup>this.form.controls['addressForm']).controls[controlName].valid;
   }
 
 }
