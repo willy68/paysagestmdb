d3f9e2dc1483ff241ff1e789f5531899321f881d
William Lety <william.lety@gmail.com>
2020-03-27 17:30:14 +0100

    Modify header to X-Requested-With

--- src/app/interceptor/xhr.interceptor.ts
+++ src/app/interceptor/xhr.interceptor.ts
@@ -2,17 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { AuthenticationService } from '../services';
-
 @Injectable()
 export class XhrInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor() { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     request = request.clone({
       setHeaders: {
-        HTTP_X_REQUESTED_WITH: 'XMLHttpRequest'
+        'X-Requested-With': 'XMLHttpRequest'
       }
     });
     return next.handle(request);
